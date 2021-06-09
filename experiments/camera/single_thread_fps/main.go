53c655ebc6d8fcbf9f2c255f6d0d3c7d99e0e9dc
Hari Anugrah <code.anugrah@gmail.com>
2021-06-10 01:02:23 +0800

    experiment

---
+++ experiments/camera/single_thread_fps/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"log"
+	"strconv"
+	"time"
+
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	log.Println("FPS camera test")
+
+	webcam, err := gocv.VideoCaptureDevice(0)
+	if err != nil {
+		log.Fatalf("Failed to open video capture device")
+	}
+	defer webcam.Close()
+
+	prevWindow := gocv.NewWindow("Preview Window")
+	defer prevWindow.Close()
+
+	img := gocv.NewMat()
+	defer img.Close()
+
+	for {
+		startTime := time.Now()
+
+		webcam.Read(&img)
+
+		fpsPos := image.Point{X: 10, Y: 40}
+		fpsColor := color.RGBA{255, 255, 255, 0}
+		elapsed := time.Now().Sub(startTime)
+		fps := 1000.0 / elapsed.Milliseconds()
+		elapsedStr := "FPS: " + strconv.Itoa(int(fps))
+
+		gocv.PutText(&img, elapsedStr, fpsPos, gocv.FontHersheyPlain, 1.5, fpsColor, 1)
+
+		prevWindow.IMShow(img)
+
+		keyPressed := prevWindow.WaitKey(1)
+		if keyPressed == 'q' {
+			break
+		}
+	}
+
+}
