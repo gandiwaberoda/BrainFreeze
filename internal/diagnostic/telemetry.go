f77999e387916d51f9540284c6a4d4c8863a8a8a
Hari Anugrah <code.anugrah@gmail.com>
2021-06-15 21:50:54 +0800

    working on state telemetry

--- internal/diagnostic/telemetry.go
+++ internal/diagnostic/telemetry.go
@@ -1,1 +1,43 @@
 package diagnostic
+
+import (
+	"time"
+
+	"harianugrah.com/brainfreeze/pkg/models/configuration"
+	"harianugrah.com/brainfreeze/pkg/telepathy"
+)
+
+type Telemetry struct {
+	isRunning bool
+	ticker    *time.Ticker
+	tele      *telepathy.Telepathy
+	config    *configuration.FreezeConfig
+}
+
+func CreateNewTelemetry(telepathy *telepathy.Telepathy, config *configuration.FreezeConfig) *Telemetry {
+	return &Telemetry{
+		isRunning: false,
+		tele:      telepathy,
+		config:    config,
+	}
+}
+
+func worker(t *Telemetry) {
+	t.ticker = time.NewTicker(time.Second / t.config.Diagnostic.TelemetryHz)
+
+	for {
+		<-t.ticker.C
+		tele := *t.tele
+		tele.Send(t.)
+	}
+}
+
+func (t *Telemetry) Start() (bool, error) {
+	go worker(t)
+	return true, nil
+}
+
+func (t *Telemetry) Stop() (bool, error) {
+	t.ticker.Stop()
+	return true, nil
+}
