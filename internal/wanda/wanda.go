c527268d1bb324508539914879a1932788833e8f
Hari Anugrah <code.anugrah@gmail.com>
2021-06-24 09:32:58 +0800

    use forward camera on wanda

--- internal/wanda/wanda.go
+++ internal/wanda/wanda.go
@@ -17,12 +17,13 @@ import (
 )
 
 type WandaVision struct {
-	isRunning  bool
-	conf       *configuration.FreezeConfig
-	topCamera  *acquisition.TopCameraAcquisition
-	ballNarrow *ball.NarrowHaesveBall
-	state      *state.StateAccess
-	fpsHsv     *diagnostic.FpsGauge
+	isRunning     bool
+	conf          *configuration.FreezeConfig
+	topCamera     *acquisition.TopCameraAcquisition
+	forwardCamera *acquisition.ForwardCameraAcquisition
+	ballNarrow    *ball.NarrowHaesveBall
+	state         *state.StateAccess
+	fpsHsv        *diagnostic.FpsGauge
 }
 
 func NewWandaVision(conf *configuration.FreezeConfig, state *state.StateAccess) *WandaVision {
@@ -37,6 +38,9 @@ func NewWandaVision(conf *configuration.FreezeConfig, state *state.StateAccess) 
 var hsvWin = gocv.NewWindow("HSV")
 var rawWin = gocv.NewWindow("Post Processed")
 
+var fHsvWin = gocv.NewWindow("Forward HSV")
+var fRawWin = gocv.NewWindow("Forward Post Processed")
+
 func worker(w *WandaVision) {
 	topCenter := image.Point{
 		w.conf.Camera.MidpointRad,
@@ -46,23 +50,32 @@ func worker(w *WandaVision) {
 	warnaNewest := color.RGBA{0, 255, 0, 0}
 	warnaLastKnown := color.RGBA{0, 0, 255, 0}
 
-	frame := gocv.NewMat()
-	hsvFrame := gocv.NewMat()
-	defer hsvFrame.Close()
+	topFrame := gocv.NewMat()
+	topHsv := gocv.NewMat()
+	defer topHsv.Close()
+
+	forwardFrame := gocv.NewMat()
+	// forwardHsv := gocv.NewMat()
+	defer forwardFrame.Close()
 
 	var latestKnownBallDetection models.DetectionObject
 	latestKnownBallSet := false
 
 	for {
-		w.topCamera.Read(&frame)
 		w.fpsHsv.Tick()
 
-		gocv.CvtColor(frame, &hsvFrame, gocv.ColorBGRToHSV)
+		w.topCamera.Read(&topFrame)
+		w.forwardCamera.Read(&forwardFrame)
+
+		gocv.CvtColor(topFrame, &topHsv, gocv.ColorBGRToHSV)
+		// gocv.CvtColor(forwardFrame, &forwardHsv, gocv.ColorBGRToHSV)
+
 		// Blur
-		gocv.GaussianBlur(hsvFrame, &hsvFrame, image.Point{7, 7}, 0, 0, gocv.BorderDefault)
+		gocv.GaussianBlur(topHsv, &topHsv, image.Point{7, 7}, 0, 0, gocv.BorderDefault)
+		// gocv.GaussianBlur(forwardHsv, &forwardHsv, image.Point{7, 7}, 0, 0, gocv.BorderDefault)
 
 		// Ball
-		narrowBallFound, narrowBallRes := w.ballNarrow.Detect(&hsvFrame)
+		narrowBallFound, narrowBallRes := w.ballNarrow.Detect(&topHsv)
 		if narrowBallFound {
 			// TODO: Perlu lakukan classifier
 
@@ -78,15 +91,15 @@ func worker(w *WandaVision) {
 
 				transform := obj.AsTransform(w.conf)
 
-				gocv.Rectangle(&frame, narrowBallRes[0].Bbox, warnaNewest, 3)
-				gocv.Circle(&frame, obj.Midpoint, obj.OuterRad, warnaNewest, 2)
+				gocv.Rectangle(&topFrame, narrowBallRes[0].Bbox, warnaNewest, 3)
+				gocv.Circle(&topFrame, obj.Midpoint, obj.OuterRad, warnaNewest, 2)
 				// Origin to Ball Line
-				gocv.Line(&frame, w.conf.Camera.Midpoint, obj.Midpoint, warnaNewest, 2)
+				gocv.Line(&topFrame, w.conf.Camera.Midpoint, obj.Midpoint, warnaNewest, 2)
 
 				w.state.UpdateBallTransform(transform)
 				latestKnownBallDetection = obj
 			} else {
-				gocv.Line(&frame, w.conf.Camera.Midpoint, latestKnownBallDetection.Midpoint, warnaLastKnown, 2)
+				gocv.Line(&topFrame, w.conf.Camera.Midpoint, latestKnownBallDetection.Midpoint, warnaLastKnown, 2)
 			}
 
 		} else {
@@ -104,17 +117,27 @@ func worker(w *WandaVision) {
 
 		// FPS Gauge
 		fpsText := fmt.Sprint(w.fpsHsv.Read(), "FPS")
-		gocv.PutText(&hsvFrame, fpsText, image.Point{10, 60}, gocv.FontHersheyPlain, 5, color.RGBA{0, 255, 255, 0}, 3)
+		gocv.PutText(&topHsv, fpsText, image.Point{10, 60}, gocv.FontHersheyPlain, 5, color.RGBA{0, 255, 255, 0}, 3)
 		w.state.UpdateFpsHsv(w.fpsHsv.Read())
 
 		mainthread.Call(func() {
-			rawWin.IMShow(frame)
+			rawWin.IMShow(topFrame)
 			if keyPressed := rawWin.WaitKey(1); keyPressed == 'q' {
 				return
 			}
 
-			hsvWin.IMShow(hsvFrame)
+			hsvWin.IMShow(topHsv)
 			if keyPressed := hsvWin.WaitKey(1); keyPressed == 'q' {
+				return
+			}
+
+			// fHsvWin.IMShow(forwardHsv)
+			// if keyPressed := fHsvWin.WaitKey(1); keyPressed == 'q' {
+			// 	return
+			// }
+
+			fRawWin.IMShow(forwardFrame)
+			if keyPressed := fRawWin.WaitKey(1); keyPressed == 'q' {
 				return
 			}
 
@@ -125,6 +148,9 @@ func worker(w *WandaVision) {
 func (w *WandaVision) Start() {
 	w.topCamera = acquisition.CreateTopCameraAcquisition(w.conf)
 	w.topCamera.Start()
+
+	w.forwardCamera = acquisition.NewForwardCameraAcquisition(w.conf)
+	w.forwardCamera.Start()
 
 	w.fpsHsv.Start()
 
