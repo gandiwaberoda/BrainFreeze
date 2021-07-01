17a3a178187ac7f6b187d56395368503b29132bc
Hari Anugrah <code.anugrah@gmail.com>
2021-07-01 13:37:03 +0800

    experimenting with goalpost

--- experiments/camera/goalpost/goalpostdetect.go
+++ experiments/camera/goalpost/goalpostdetect.go
@@ -12,35 +12,24 @@ import (
 )
 
 func main() {
-	vc1, err1 := gocv.VideoCaptureDevice(1)
-	vc2, err2 := gocv.VideoCaptureDevice(0)
+	vc1, err1 := gocv.VideoCaptureDevice(0)
 
 	if err1 != nil {
 		panic(err1)
 	}
 
-	if err2 != nil {
-		panic(err2)
-	}
-
 	f1 := gocv.NewMat()
 	defer f1.Close()
-
-	f2 := gocv.NewMat()
-	defer f2.Close()
 
 	win1 := gocv.NewWindow("F1")
 	win2 := gocv.NewWindow("F2")
 
 	fps1 := diagnostic.NewFpsGauge()
 	fps1.Start()
-	fps2 := diagnostic.NewFpsGauge()
-	fps2.Start()
 
 	white := color.RGBA{128, 255, 0, 0}
 
 	mutex1 := sync.Mutex{}
-	mutex2 := sync.Mutex{}
 
 	fmt.Println("A")
 	go func() {
@@ -52,51 +41,47 @@ func main() {
 			mutex1.Unlock()
 		}
 	}()
-	fmt.Println("B")
-	go func() {
-		for {
-			mutex2.Lock()
-			vc2.Read(&f2)
-			gocv.PutText(&f2, "FPS B: "+strconv.Itoa(fps2.Read()), image.Point{10, 50}, gocv.FontHersheyPlain, 3, white, 2)
-			fps2.Tick()
-			mutex2.Unlock()
-		}
-	}()
 
 	fmt.Println("C")
 	// <-time.After(time.Second * 2)
 
 	for {
-		if !f1.Empty() && !f2.Empty() {
+		if !f1.Empty() {
 			fmt.Println("YES")
 			break
 		} else {
 			if f1.Empty() {
 				fmt.Println("F1 Empty")
 			}
-			if !f2.Empty() {
-				fmt.Println("F2 Empty")
-			}
 		}
 	}
 
 	fmt.Println("D")
+
+	upper := gocv.NewScalar(166, 51, 255, 1)
+	lower := gocv.NewScalar(0, 0, 244, 0)
+
+	erodeMat := gocv.Ones(27, 27, gocv.MatTypeCV8UC1)
+	dilateMat := gocv.Ones(40, 40, gocv.MatTypeCV8UC1)
+	defer erodeMat.Close()
+	defer dilateMat.Close()
+
+	hsv := gocv.NewMat()
 	for {
-		// mutex1.Lock()
-		// mutex2.Lock()
+		gocv.CvtColor(f1, &hsv, gocv.ColorBGRToHSV)
+		gocv.InRangeWithScalar(hsv, lower, upper, &hsv)
+
+		gocv.Erode(hsv, &hsv, erodeMat)
+		gocv.Dilate(hsv, &hsv, dilateMat)
+		// // gocv.Threshold(gray, &gray, 170, 255, gocv.ThresholdBinary)
 
 		if !f1.Empty() {
 			win1.IMShow(f1)
-			win1.WaitKey(1)
-		}
+			win2.IMShow(hsv)
 
-		if !f2.Empty() {
-			win2.IMShow(f2)
+			win1.WaitKey(1)
 			win2.WaitKey(1)
 		}
-
-		// mutex1.Unlock()
-		// mutex2.Unlock()
 	}
 
 }
