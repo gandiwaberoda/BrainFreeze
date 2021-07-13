173195b359e2c19b5d721ad75cb23cc138172538
Hari Anugrah <code.anugrah@gmail.com>
2021-07-13 21:44:56 +0800

    create sensor model

--- experiments/occupancygrid/show.go
+++ experiments/occupancygrid/show.go
@@ -10,7 +10,8 @@ import (
 )
 
 var (
-	img image.Image
+	img   image.Image
+	robot = NewSensorModel()
 )
 
 const (
@@ -34,18 +35,7 @@ func setup() {
 
 }
 
-func Polar2Cartesian(deg, rad float64) (x, y float64) {
-	radian := ((deg * -1) - 90) * math.Pi / 180
-	x = rad * math.Cos(radian)
-	y = rad * math.Sin(radian)
-	return
-}
-
 func draw() {
-	fov := 140.0
-	fovStep := 15.0
-	rad := 300.0
-
 	p5.DrawImage(img, 0, 0)
 	mouseX := p5.Event.Mouse.Position.X
 	mouseY := p5.Event.Mouse.Position.Y
@@ -53,35 +43,12 @@ func draw() {
 		return
 	}
 
-	// fmt.Println("X:", int(mouseX), "Y:", int(mouseY), "\t", px, "\t")
-
 	p5.Push()
-	fmt.Println("Gambar")
-	for i := -fov; i <= fov; i += fovStep {
-		var endX, endY float64
-		lX, lY := Polar2Cartesian(i, rad)
-		lX += mouseX
-		lY += mouseY
-		p5.StrokeWidth(1)
-		p5.Stroke(color.RGBA{50, 50, 50, 100})
-		p5.Line(mouseX, mouseY, lX, lY)
-
-		p5.StrokeWidth(5)
+	robot.SenseFromImage(img, image.Point{int(mouseX), int(mouseY)})
+	for k, v := range robot.Reading {
 		p5.Stroke(color.RGBA{128, 128, 255, 255})
-		for r := 10.0; r <= rad; r++ {
-			endX, endY = Polar2Cartesian(i, r)
-			endX += mouseX
-			endY += mouseY
-
-			px := img.At(int(endX), int(endY))
-			// fmt.Println(px.RGBA())
-			if IsWhite(px) {
-				// p5.Line(mouseX, mouseY, endX+rand.Float64()*30, endY+rand.Float64()*30)
-				p5.Line(mouseX, mouseY, endX, endY)
-				break
-			}
-		}
-		// fmt.Println(endX, "\t", endY)
+		x, y := Polar2Cartesian(k, v.ClosestPoint)
+		p5.Line(mouseX, mouseY, x+mouseX, y+mouseY)
 	}
 	p5.Pop()
 
@@ -112,13 +79,61 @@ func IsWhite(ac color.Color) bool {
 	return r == max && g == max && b == max
 }
 
-// type LidarReading struct {
-// 	SensorRot    float64
-// 	ClosestPoint float64
-// }
-// type SensorModel struct {
-// 	NumSensor   int
-// 	MinRotation float64
-// 	MaxRotation float64
-// 	Reading     []LidarReading
-// }
+type LidarReading struct {
+	ClosestPoint float64
+}
+type SensorModel struct {
+	numSensor   int
+	minRotation float64
+	maxRotation float64
+	deadZoneRad float64
+	maxDistance float64
+	Reading     map[float64]LidarReading
+}
+
+func NewSensorModel() SensorModel {
+	reading := make(map[float64]LidarReading)
+	min := -130.0
+	max := 130.0
+	num := 14
+	sen_range := math.Abs(min) + math.Abs(max)
+	sen_step := sen_range / float64(num)
+
+	for i := min; i <= max; i += sen_step {
+		reading[i] = LidarReading{}
+	}
+
+	return SensorModel{
+		numSensor:   num,
+		minRotation: min,
+		maxRotation: max,
+		maxDistance: 320,
+		Reading:     reading,
+		deadZoneRad: 10,
+	}
+}
+
+func (rob *SensorModel) SenseFromImage(img image.Image, worldPos image.Point) {
+	for k := range rob.Reading {
+		newReading := LidarReading{}
+		for r := rob.deadZoneRad; r <= rob.maxDistance; r++ {
+			endX, endY := Polar2Cartesian(k, r)
+			endX += float64(worldPos.X)
+			endY += float64(worldPos.Y)
+
+			px := img.At(int(endX), int(endY))
+			if IsWhite(px) {
+				newReading.ClosestPoint = r
+				break
+			}
+		}
+		rob.Reading[k] = newReading
+	}
+}
+
+func Polar2Cartesian(deg, rad float64) (x, y float64) {
+	radian := ((deg * -1) - 90) * math.Pi / 180
+	x = rad * math.Cos(radian)
+	y = rad * math.Sin(radian)
+	return
+}
